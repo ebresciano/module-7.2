04fb010885757995c2dfe7290a897efe12d198f7
Eva <Bresciano>
2016-05-17 09:31:09 -0600

    stretch

---
+++ Stretch Problem 2.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var X = 5
+var Y = 100
+
+
+for number in 0...Y {
+    print (X)
+}
+
+func primeNumber(X: Int, Y: Int) {
+    for number in 0...Y {
+        if X == X % 5 {
+        
+        print (X)
+    }
+}
+
+}
+
