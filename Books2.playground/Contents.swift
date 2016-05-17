dbbe7020790dba99b7ac52cea5eecbf32d47d731
Eva <Bresciano>
2016-05-17 09:04:19 -0600

    chalenges

---
+++ Books2.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+ class Book {
+    
+    let title: String
+    let author: String
+    let genre: String
+    
+    init(title: String, author: String, genre: String){
+    
+    self.title = title
+    self.author = author
+    self.genre = genre
+}
+}
+
+var bookShelf: [] {
+
+}
---
+++ Cars2.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+class Car {
+    let make: String
+    let model: String
+    let year: String
+    let vin: String
+    
+    init(make: String, model: String, year: String, vin: String) {
+        self.make = make
+        self.model = model
+        self.year = year
+        self.vin = vin 
+        
+    }
+    
+    
+}
+
